7842314dc78b773ccde1087b4cc7db0023ae03e0
Sebastian Kropp <skropp@learningobjects.com>
2016-04-14 11:28:42 -0400

    added function

--- mock-event-generator.js
+++ mock-event-generator.js
@@ -8,50 +8,54 @@ var studentIds = new Set()
 var events = []
 var uuidG = 0
 
-for (let z in data_worksheet) {
-  if (z[0] === '!') continue
+function generateSystemCreateUser () {
+  for (let z in data_worksheet) {
+    if (z[0] === '!') continue
 
-  // parse out the column, row, and value
-  var col = z.substring(0, 1)
-  var row = parseInt(z.substring(1))
-  var value = data_worksheet[z].v
+    // parse out the column, row, and value
+    var col = z.substring(0, 1)
+    var row = parseInt(z.substring(1))
+    var value = data_worksheet[z].v
 
-  if (!data[row]) data[row] = {}
-  data[row][headers[col]] = value
+    if (!data[row]) data[row] = {}
+    data[row][headers[col]] = value
 
-  // store header names
-  if (row === 1) {
-    headers[col] = value
-    continue
+    // store header names
+    if (row === 1) {
+      headers[col] = value
+      continue
+    }
   }
+
+  // console.log(data)
+
+  for (let d in data) {
+    var student_id = data[d].syStudentID
+    studentIds.add(student_id)
+  }
+
+  studentIds.forEach(function (value) {
+    let event = {
+      uuid: uuidG,
+      time: '',
+      type: 'system.create.user',
+      source: 'lou',
+      objVal: {
+        person_id: value,
+        username: 'UN' + value,
+        email: 'srahman+' + value + '@learningobjects.com',
+        given_name: 'FN' + value,
+        middle_name: null,
+        family_name: 'LN' + value
+      },
+      objValOld: {}
+
+    }
+    events.push(event)
+    uuidG++
+  })
+
+  console.log(events)
 }
 
-// console.log(data)
-
-for (let d in data) {
-  var student_id = data[d].syStudentID
-  studentIds.add(student_id)
-}
-
-studentIds.forEach(function (value) {
-  let event = {
-    uuid: uuidG,
-    time: '',
-    type: 'system.create.user',
-    source: 'lou',
-    objVal: {
-      person_id: value,
-      username: 'UN' + value,
-      email: 'srahman+' + value + '@learningobjects.com',
-      given_name: 'FN' + value,
-      middle_name: null,
-      family_name: 'LN' + value
-    },
-    objValOld: {}
-
-  }
-  events.push(event)
-  uuidG++
-})
-
-console.log(events)
+generateSystemCreateUser()
