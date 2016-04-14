b5e2a875ff945a3108b4730c5dcdd2ce901e849d
Sebastian Kropp <skropp@learningobjects.com>
2016-04-14 10:59:15 -0400

    quick refactor

---
+++ mock-event-generator.js
@@ -0,0 +1,57 @@
+var XLSX = require('xlsx')
+var workbook = XLSX.readFile('test.xlsx')
+var first_worksheet = workbook.SheetNames[0]
+var data_worksheet = workbook.Sheets[first_worksheet]
+var headers = {}
+var data = []
+var studentIds = new Set()
+var events = []
+var uuidG = 0
+
+for (z in data_worksheet) {
+  if (z[0] === '!') continue
+
+  // parse out the column, row, and value
+  var col = z.substring(0, 1)
+  var row = parseInt(z.substring(1))
+  var value = data_worksheet[z].v
+
+  if (!data[row]) data[row] = {}
+  data[row][headers[col]] = value
+
+  // store header names
+  if (row == 1) {
+    headers[col] = value
+    continue
+  }
+}
+
+// console.log(data)
+
+for (d in data) {
+  var student_id = data[d].syStudentID
+  studentIds.add(student_id)
+}
+
+studentIds.forEach(function (value) {
+  event = {
+    uuid: uuidG,
+    time: '',
+    type: 'system.create.user',
+    source: 'lou',
+    objVal: {
+      person_id: value,
+      username: 'UN' + value,
+      email: 'srahman+' + value + '@learningobjects.com',
+      given_name: 'FN' + value,
+      middle_name: null,
+      family_name: 'LN' + value
+    },
+    objValOld: {}
+
+  }
+  events.push(event)
+  uuidG++
+})
+
+console.log(events)
