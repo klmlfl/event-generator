a2a3fbfcf837de01ee803a4277562de12886183f
Shaban <srahman@learningobjects.com>
2016-04-28 15:51:25 -0400

    Create new workbook with specified worksheets

---
+++ create-worksheets.js
@@ -0,0 +1,24 @@
+'use strict'
+const XLSX = require('xlsx')
+
+
+function createWorksheetsOutput() {
+//set up workbook objects
+var wb = {}
+wb.Sheets = {};
+wb.Props = {};
+wb.SSF = {};
+wb.SheetNames = [];
+
+// create worksheet
+var ws = {}
+
+// add worksheet to workbook
+wb.SheetNames.push("Person", "Course", "Course_Section", "Enrollment");
+wb.Sheets["Person"] = ws;
+
+//write file
+XLSX.writeFile(wb, 'test_output.xlsx');
+}
+
+createWorksheetsOutput()
