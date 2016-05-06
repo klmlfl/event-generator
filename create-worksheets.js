b2aa3a13e5a1768fac2ee4011d92b2abf7421259
Shaban <srahman@learningobjects.com>
2016-05-06 14:41:15 -0400

    create-persons and create-courses json

--- create-worksheets.js
+++ create-worksheets.js
@@ -1,24 +1,25 @@
 'use strict'
 const XLSX = require('xlsx')
 
+function createWorksheets () {
+  // set up workbook objects
+  var wb = {}
+  wb.Sheets = {}
+  wb.Props = {}
+  wb.SSF = {}
+  wb.SheetNames = []
 
-function createWorksheetsOutput() {
-//set up workbook objects
-var wb = {}
-wb.Sheets = {};
-wb.Props = {};
-wb.SSF = {};
-wb.SheetNames = [];
+  // create worksheet
+  var ws = {}
 
-// create worksheet
-var ws = {}
+  // add worksheet to workbook
+  wb.SheetNames.push('Person', 'Course', 'Course Section', 'Enrollment')
+  wb.Sheets['Person'] = ws
 
-// add worksheet to workbook
-wb.SheetNames.push("Person", "Course", "Course Section", "Enrollment");
-wb.Sheets["Person"] = ws;
-
-//write file
-XLSX.writeFile(wb, 'mock_files/output/test_worksheets.xlsx');
+  // write file
+  XLSX.writeFile(wb, 'mock_files/output/test_worksheets.xlsx')
 }
 
-createWorksheetsOutput()
+createWorksheets()
+
+
